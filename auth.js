d67d8975f8178e86ed3db850a3193aa970cf2ed4
Rupadharshan S <ae25b048@smail.iitm.ac.in>
2026-03-16 23:32:07 +0530

    Implement authentication functions in auth.js

---
+++ auth.js
@@ -0,0 +1,27 @@
+import { auth } from "./firebase.js";
+
+import{
+createUserWithEmailAndPassword,
+signInWithEmailAndPassword,
+GoogleAuthProvider,
+signInWithPopup,
+onAuthStateChanged
+}
+from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+
+export function signup(email,password){
+return createUserWithEmailAndPassword(auth,email,password);
+}
+
+export function login(email,password){
+return signInWithEmailAndPassword(auth,email,password);
+}
+
+export function googleLogin(){
+const provider=new GoogleAuthProvider();
+return signInWithPopup(auth,provider);
+}
+
+export function listenAuth(callback){
+onAuthStateChanged(auth,callback);
+}
