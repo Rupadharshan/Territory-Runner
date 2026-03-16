38ae62a2a1767a1be1ff91b512d292aa1921867a
Rupadharshan S <ae25b048@smail.iitm.ac.in>
2026-03-16 23:31:26 +0530

    Initialize Firebase app and services
    Set up Firebase configuration and initialize services.

---
+++ firebase.js
@@ -0,0 +1,22 @@
+import { initializeApp } from
+"https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+
+import { getAuth } from
+"https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+
+import { getFirestore } from
+"https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
+
+const firebaseConfig={
+apiKey:"YOUR_KEY",
+authDomain:"YOUR_PROJECT.firebaseapp.com",
+projectId:"YOUR_PROJECT",
+storageBucket:"YOUR_PROJECT.appspot.com",
+messagingSenderId:"XXXX",
+appId:"XXXX"
+};
+
+const app=initializeApp(firebaseConfig);
+
+export const auth=getAuth(app);
+export const db=getFirestore(app);
